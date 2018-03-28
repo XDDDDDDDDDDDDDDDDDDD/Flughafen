dc7ae347f7e48c3d481994e83d3a480a16611649
Kevin <Kevin@192.168.2.112>
2018-03-28 18:32:01 +0200

    Projekt erstellt. Erste Seite Body

---
+++ src/main/Home.java
@@ -0,0 +1,49 @@
+package main;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/Home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Home() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	PrintWriter writer = response.getWriter();
+    	
+    	writer.println("<html>");
+    	writer.println("<head><title>Flughafen</title></head>");
+    	writer.println("<body>");
+    	writer.println("<body>");
+    	writer.println("</html>");
+    		
+    	writer.close();			
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
