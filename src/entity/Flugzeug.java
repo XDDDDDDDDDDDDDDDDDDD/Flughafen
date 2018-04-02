b10bc1a9b28b3ce4fecf28e693a25b74e3785a1f
Kevin <Kevin@192.168.2.112>
2018-04-02 22:00:26 +0200

    css angepasst und in jede seite eingebettet. nach redirect funktioniert css jedoch nicht mehr

--- src/entity/Flugzeug.java
+++ src/entity/Flugzeug.java
@@ -14,7 +14,6 @@ public class Flugzeug implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 
 	private String code;
--- src/entity/Kunde.java
+++ src/entity/Kunde.java
@@ -14,7 +14,6 @@ public class Kunde implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 
 	private String nachname;
